a3e6054f53358ac0ccd8d63b98bcf6857a29e804
0Omaaar <ellhotriomar@gmail.com>
2023-08-15 21:39:11 +0100

    54

--- school_system/app/Http/Controllers/Teachers/dashboard/QuizzController.php
+++ school_system/app/Http/Controllers/Teachers/dashboard/QuizzController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Teachers\dashboard;
 use App\Http\Controllers\Controller;
 use App\Models\Classroom;
 use App\Models\Grade;
+use App\Models\Question;
 use App\Models\Quizze;
 use App\Models\Section;
 use App\Models\Subject;
@@ -39,7 +40,7 @@ class QuizzController extends Controller
             $quizzes->section_id = $request->section_id;
             $quizzes->teacher_id = auth()->user()->id;
             $quizzes->save();
-            
+
             return redirect()->route('quizzes.index');
         } catch (\Exception $e) {
             return redirect()->back()->with(['error' => $e->getMessage()]);
@@ -49,7 +50,9 @@ class QuizzController extends Controller
 
     public function show(string $id)
     {
-        //
+        $questions = Question::where('quizze_id', $id)->get();
+        $quizz = Quizze::findorFail($id);
+        return view('pages.Teachers.dashboard.Questions.index', compact('questions', 'quizz'));
     }
 
 
@@ -57,20 +60,39 @@ class QuizzController extends Controller
     {
         $quizz = Quizze::findorFail($id);
         $data['grades'] = Grade::all();
-        $data['subjects'] = Subject::where('teacher_id',auth()->user()->id)->get();
+        $data['subjects'] = Subject::where('teacher_id', auth()->user()->id)->get();
         return view('pages.Teachers.dashboard.Quizzes.edit', $data, compact('quizz'));
     }
 
 
-    public function update(Request $request, string $id)
+    public function update(Request $request)
     {
-        //
+        try {
+            $quizz = Quizze::findorFail($request->id);
+            $quizz->name = $request->name;
+            $quizz->subject_id = $request->subject_id;
+            $quizz->grade_id = $request->grade_id;
+            $quizz->classroom_id = $request->classroom_id;
+            $quizz->section_id = $request->section_id;
+            $quizz->teacher_id = auth()->user()->id;
+            $quizz->save();
+
+            return redirect()->route('quizzes.index');
+        } catch (\Exception $e) {
+            return redirect()->back()->with(['error' => $e->getMessage()]);
+        }
     }
 
 
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        try {
+            Quizze::destroy($id);
+
+            return redirect()->back();
+        } catch (\Exception $e) {
+            return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+        }
     }
 
     public function Get_classrooms($id)
